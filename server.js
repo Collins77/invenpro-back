4414be211b4ebc05edae62fe1a9232c3351a756f
Collins77 <collinsmuemah@gmail.com>
2025-10-11 09:13:49 +0300

    updat

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const PORT = process.env.PORT || 5000;
 // const cors = require('cors');
 // app.use(cors({ origin: 'http://localhost:5173' }));
 
+app.use(cors({
+  origin: 'https://invenpro-front.vercel.app/',
+  credentials: true
+}));
+
 db.sequelize.sync({ alter: true }).then(() => {
   console.log('Database synced');
   app.listen(PORT, '0.0.0.0', () => {
